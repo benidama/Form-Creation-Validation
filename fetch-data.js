2744cfa63da757b609eaa4103ba7fc5fe546081b
benidama <benidama02@gmail.comls>
2024-10-13 16:15:15 +0000

    Fetching Data from an API and Displaying It

--- fetch-data.js
+++ fetch-data.js
@@ -1,28 +1,24 @@
-async function fetchUserData() {
+const fetchUserData = async function () {
   const apiUrl = "https://jsonplaceholder.typicode.com/users";
   const dataContainer = document.getElementById("api-data");
-  const response = document.getElementById("api-data");
+  try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    dataContainer.innerHTML = "";
+    // console.log(users)
+    const userList = document.createElement("ul");
+    users.forEach((user) => {
+      const myUser = user.name;
+      const newElement = document.createElement("li");
+      newElement.textContent = myUser;
+      userList.appendChild(newElement);
+      dataContainer.appendChild(userList);
+    });
+  } catch (error) {
+    error.textContent = "Failed to load user data.";
+  }
+};
 
-  let myPromise = new Promise(function (resolve) {
-    let req = new XMLHttpRequest();
-    req.open("GET", "apiUrl");
-    req.onload = function () {
-      try {
-        if (req.status == 200) {
-          resolve(req.response);
-        }
-      } catch {
-        resolve("Failed to load user data.");
-      }
-    };
-    req.send();
-  });
-  response.innerHTML = await myPromise;
-  const user = await response.json();
-  dataContainer.innerHTML = "";
-  const userList = document.createElement("ul");
-  user.forEach((e) => "<li>e</li>");
-  userList.appendChild(dataContainer);
-}
-
-document.addEventListener("DOMContentLoaded", fetchUserData);
+document.addEventListener("DOMContentLoaded", () => {
+  fetchUserData();
+});
